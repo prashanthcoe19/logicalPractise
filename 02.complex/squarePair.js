1906a5e6a5505cccf2848d8ed68e463122fe3143
Prashant <prashant.niroula67@gmail.com>
2020-05-25 15:47:14 +0545

    square pair

---
+++ 02.complex/squarePair.js
@@ -0,0 +1,18 @@
+const squarePair = (n) => {
+  let x = n.length;
+  console.log(n[1]);
+  console.log(x);
+  for (i = 0; i <= (x * (x - 1)) / 2; i++) {
+    for (j = 0; j <= (x * (x - 1)) / 2; j++) {
+      sum = n[i] + n[j];
+      squareRoot = Math.sqrt(sum);
+      if (Number.isInteger(squareRoot) == true) {
+        if (n[i] < n[j]) {
+          console.log('Square Pairs:', n[i], n[j]);
+        }
+      }
+    }
+  }
+};
+
+squarePair([11, 5, 4, 20]);
